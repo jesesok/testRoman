136c298d9fdc63ae5fe0d9ac20d0731be9fff05f
NikitaMoiseev <jeses47@gmail.com>
2019-04-04 20:12:25 +0300

    On branch master  Changes to be committed: 	new file:   .idea/vcs.xml 	modified:   .idea/workspace.xml 	modified:   CMakeLists.txt 	modified:   cmake-build-debug/CMakeFiles/Makefile.cmake 	modified:   cmake-build-debug/CMakeFiles/clion-log.txt 	new file:   cmake-build-debug/CMakeFiles/testRom.dir/DependInfo.cmake 	new file:   cmake-build-debug/CMakeFiles/testRom.dir/build.make 	new file:   cmake-build-debug/CMakeFiles/testRom.dir/cmake_clean.cmake 	new file:   cmake-build-debug/CMakeFiles/testRom.dir/depend.make 	new file:   cmake-build-debug/CMakeFiles/testRom.dir/flags.make 	new file:   cmake-build-debug/CMakeFiles/testRom.dir/progress.make 	modified:   cmake-build-debug/CMakeFiles/testRoman.dir/main.cpp.obj 	modified:   cmake-build-debug/CMakeFiles/testRoman.dir/objects.a 	modified:   cmake-build-debug/testRoman.exe 	modified:   main.cpp

--- main.cpp
+++ main.cpp
@@ -5,27 +5,37 @@
 using namespace std;
 
 int main() {
-    srand(time(NULL));
-    cout << "Guess number" << endl;
-    int a;
-    cout << "Ent num" << endl;
-    cin >> a;
-    cout << "Here we go" << endl;
-    cout << "ent preddiction num" << endl;
-    int c;
-    int i=1;
-    cin >> c;
-    while(c!=a){
-        if (c<a){
-            cout << "ur num is lover" << endl;
-            cin >> c;
+    cout << "\t RASHODNIK 228 BY DJ VASYAN" << endl;
+    int menuchoose;
+    int income=0;
+    int food=0;
+    int clean=0;
+    int balance = income - food - clean;
+     while (menuchoose != 0) {
+        cout << "\t \t MENU" << endl;
+        cout << "1. Income " << income << endl;
+        cout << "2. Food " << food << endl;
+        cout << "3. Cleaning products " << clean << endl;
+        cout << "Now your balance  " << balance << endl;
+        cout << "If you want out, enter 0" << endl;
+        cin >> menuchoose;
+        switch (menuchoose) {
+            case 1:
+                cout << "1. Income" << endl;
+                cin >> income;
+                break;
+            case 2:
+                cout << "2. Food" << endl;
+                cin >> food;
+                break;
+            case 3:
+                cout << "3. Cleaning products" << endl;
+                cin >> clean;
+                break;
+            deafult:
+                cout << "You enter wrong item" << endl;
+                break;
         }
-        else if(c>a){
-            cout << "ur num is upper" << endl;
-            cin >> c;
-        }
-        i++;
-    }
-    cout << "ur amazing! Num is " <<c<<" you tried "<<i<<" times"<< endl;
+      }
     return 0;
 }
