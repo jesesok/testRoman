c3865cf7630a7697290cc27fe32e4525b3d1278e
NikitaMoiseev <jeses47@gmail.com>
2019-04-04 22:24:48 +0300

    On branch master  Changes to be committed: 	modified:   .idea/workspace.xml 	modified:   cmake-build-debug/CMakeFiles/testRoman.dir/main.cpp.obj 	modified:   cmake-build-debug/CMakeFiles/testRoman.dir/objects.a 	modified:   cmake-build-debug/testRoman.exe 	modified:   main.cpp

--- main.cpp
+++ main.cpp
@@ -7,30 +7,36 @@ using namespace std;
 int main() {
     cout << "\t RASHODNIK 228 BY DJ VASYAN" << endl;
     int menuchoose;
+    int tempincome=0;
+    int tempfood=0;
+    int tempclean=0;
     int income=0;
     int food=0;
     int clean=0;
-    int balance = income - food - clean;
+    int balance = tempclean - tempfood;
      while (menuchoose != 0) {
         cout << "\t \t MENU" << endl;
         cout << "1. Income " << income << endl;
         cout << "2. Food " << food << endl;
-        cout << "3. Cleaning products " << clean << endl;
+        cout << "3. Cleaning products " << tempclean << endl;
         cout << "Now your balance  " << balance << endl;
         cout << "If you want out, enter 0" << endl;
         cin >> menuchoose;
         switch (menuchoose) {
             case 1:
                 cout << "1. Income" << endl;
-                cin >> income;
+                cin >> tempincome;
+                income+=tempincome;
                 break;
             case 2:
                 cout << "2. Food" << endl;
-                cin >> food;
+                cin >> tempfood;
+                food+=tempfood;
                 break;
             case 3:
                 cout << "3. Cleaning products" << endl;
                 cin >> clean;
+                clean+=tempclean;
                 break;
             default:
                 cout << "You enter wrong item" << endl;
