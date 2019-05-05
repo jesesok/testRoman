f53aae0220f25845269cde3637b515f22f22aa5a
jesesok <jeses47@gmail.com>
2019-05-05 19:43:34 +0300

    modified:   .idea/workspace.xml 	modified:   cmake-build-debug/CMakeCache.txt 	new file:   cmake-build-debug/CMakeFiles/3.14.2/CMakeCCompiler.cmake 	new file:   cmake-build-debug/CMakeFiles/3.14.2/CMakeCXXCompiler.cmake 	new file:   cmake-build-debug/CMakeFiles/3.14.2/CMakeDetermineCompilerABI_C.bin 	new file:   cmake-build-debug/CMakeFiles/3.14.2/CMakeDetermineCompilerABI_CXX.bin 	new file:   cmake-build-debug/CMakeFiles/3.14.2/CMakeRCCompiler.cmake 	new file:   cmake-build-debug/CMakeFiles/3.14.2/CMakeSystem.cmake 	new file:   cmake-build-debug/CMakeFiles/3.14.2/CompilerIdC/CMakeCCompilerId.c 	new file:   cmake-build-debug/CMakeFiles/3.14.2/CompilerIdC/a.exe 	new file:   cmake-build-debug/CMakeFiles/3.14.2/CompilerIdCXX/CMakeCXXCompilerId.cpp 	new file:   cmake-build-debug/CMakeFiles/3.14.2/CompilerIdCXX/a.exe 	modified:   cmake-build-debug/CMakeFiles/CMakeDirectoryInformation.cmake 	modified:   cmake-build-debug/CMakeFiles/CMakeOutput.log 	modified:   cmake-build-debug/CMakeFiles/Makefile.cmake 	modified:   cmake-build-debug/CMakeFiles/Makefile2 	modified:   cmake-build-debug/CMakeFiles/clion-log.txt 	modified:   cmake-build-debug/CMakeFiles/feature_tests.bin 	modified:   cmake-build-debug/CMakeFiles/testRoman.dir/build.make 	modified:   cmake-build-debug/CMakeFiles/testRoman.dir/depend.internal 	modified:   cmake-build-debug/CMakeFiles/testRoman.dir/depend.make 	modified:   cmake-build-debug/CMakeFiles/testRoman.dir/flags.make 	modified:   cmake-build-debug/CMakeFiles/testRoman.dir/link.txt 	modified:   cmake-build-debug/CMakeFiles/testRoman.dir/main.cpp.obj 	modified:   cmake-build-debug/CMakeFiles/testRoman.dir/objects.a 	modified:   cmake-build-debug/Makefile 	modified:   cmake-build-debug/testRoman.exe 	modified:   main.cpp

--- main.cpp
+++ main.cpp
@@ -1,47 +1,13 @@
 #include <iostream>
-#include <ctime>
 #include <string>
+#include <ctime>
 
 using namespace std;
 
-int main() {
-    cout << "\t RASHODNIK 228 BY DJ VASYAN" << endl;
-    int menuchoose;
-    int tempincome=0;
-    int tempfood=0;
-    int tempclean=0;
-    int income=0;
-    int food=0;
-    int clean=0;
-    int balance = tempclean - tempfood;
-     while (menuchoose != 0) {
-        cout << "\t \t MENU" << endl;
-        cout << "1. Income " << income << endl;
-        cout << "2. Food " << food << endl;
-        cout << "3. Cleaning products " << tempclean << endl;
-        cout << "Now your balance  " << balance << endl;
-        cout << "If you want out, enter 0" << endl;
-        cin >> menuchoose;
-        switch (menuchoose) {
-            case 1:
-                cout << "1. Income" << endl;
-                cin >> tempincome;
-                income+=tempincome;
-                break;
-            case 2:
-                cout << "2. Food" << endl;
-                cin >> tempfood;
-                food+=tempfood;
-                break;
-            case 3:
-                cout << "3. Cleaning products" << endl;
-                cin >> clean;
-                clean+=tempclean;
-                break;
-            default:
-                cout << "You enter wrong item" << endl;
-                break;
-        }
-      }
-    return 0;
+
+
+int main(){
+    string a="5";
+    string b="5";
+    cout<<a+b<<endl;
 }
